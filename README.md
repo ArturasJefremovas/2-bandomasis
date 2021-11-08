6ebb47ea8e2bd1b7305e3da725ef1deb960d0b39
Arturas Jefremovas <jefremovas.arturas@gmail.com>
2021-11-08 15:30:57 +0200

    [3]: json server start

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 2-bandomasis
 CRUD
+
+# Required programs
+node.js
+
+## Instalation
+npm i
+
+## Start program
+* Windows users - npm run start-windows
+* Unix/Mac users - npm run start-unix
---
+++ main.js
@@ -0,0 +1,4 @@
+const rootElement = document.querySelector('#root');
+
+const furnitureGridComponent = new FurnitureGridComponent();
+rootElement.appendChild(furnitureGridComponent.htmlElement);
